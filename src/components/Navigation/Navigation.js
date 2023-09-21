67b993b855a9e90922848f500e8c6fcca8b2a5ec
Prakash Joshi <prakash.joshi167@hotmail.com>
2023-09-21 22:59:09 +0530

    first commit

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navigation = () => {
+    return (
+        <nav style={{display:'flex' , justifyContent:'end'}}>
+            <p>Sign Out</p>
+        </nav>
+    )
+}
+
+export default Navigation;
