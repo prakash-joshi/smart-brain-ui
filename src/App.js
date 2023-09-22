3e0a9cec879304777f6fe9ded5d37bf877b39737
Prakash Joshi <prakash.joshi167@hotmail.com>
2023-09-22 20:50:44 +0530

    Added face recognition componenet for output image and clarifai api to get the data for an image url

--- src/App.js
+++ src/App.js
@@ -1,13 +1,71 @@
 import React, { Component } from 'react';
-import './App.css'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg';
+import './App.css'
+
 
 const backgroundDesigns = ["color", "ball", "lines", "thick", "circle", "cobweb", "polygon", "square", "tadpole", "fountain", "random", "custom"]
+const PAT = 'a9b4ebcc1ec54675a183c5b8e951c2eb';
+const USER_ID = 'joshi-prakash';
+const APP_ID = 'facerecognitionbrain';
+const MODEL_ID = 'face-detection';
+const returnClarifyAPIRequestOptions = (imageUrl) => {
+    // const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105'; //  this is optional
+    const IMAGE_URL = "https://samples.clarifai.com/metro-north.jpg"; //imageUrl;
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": IMAGE_URL
+                    }
+                }
+            }
+        ]
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+    return requestOptions;
+}
+
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            input: '',
+            imgUrl: '',
+        }
+    }
+
+    onInputChange = (event) => {
+        this.setState({ input: event.target.value });
+    }
+
+    onButtonSubmit = () => {
+        this.setState({ imgUrl: this.state.input });
+        fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", returnClarifyAPIRequestOptions(this.state.input))
+            .then(response => response.json())
+            .then(result => {
+                console.log('deep data : ', result.outputs[0].data.regions[0].region_info.bounding_box);
+            })
+            .catch(error => console.log('error', error));
+
+    }
     render() {
         return (
             <div>
@@ -16,7 +74,8 @@ class App extends Component {
                     <Navigation />
                     <Logo />
                     <Rank />
-                    <ImageLinkForm />
+                    <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+                    <FaceRecognition imgUrl={this.state.imgUrl} />
                 </div>
             </div>
         )
